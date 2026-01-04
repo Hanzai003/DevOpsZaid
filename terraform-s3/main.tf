e16640dae02243b68acf0e1e27f73458fe69d7f2
HP <233023@students.au.edu.pk>
2026-01-04 21:31:57 +0500

    Final state fix with correct bucket ID. Deploying final website.

--- terraform-s3/main.tf
+++ terraform-s3/main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
-}# Triggering final deployment
+}# Triggering final deployments
 
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.website_bucket.id
