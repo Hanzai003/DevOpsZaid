9f6d1fdeb18d43bcbc2a37b55087aeb1c14f64d6
HP <233023@students.au.edu.pk>
2026-01-05 13:52:13 +0500

    FINAL RESOLUTION: Deleted local state file and S3 bucket for a full clean creation plan.

--- terraform-s3/main.tf
+++ terraform-s3/main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
-}# Triggering final deployments1
+}# Triggering final deployments1s
 
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.website_bucket.id
